3c299afea754b34b7326cd59baa10086018b5573
Chandhru Soundarajan <csoundarajan@freshworks.com>
2025-03-04 19:30:32 +0530

    Basic of java script added.

---
+++ basic.js
@@ -0,0 +1,42 @@
+//Variables in java script
+//Variables store values and allow us to reference them later in the code. You can declare variables using
+
+
+// let: Declares a variable that can be reassigned.
+let name = "Chandhru";
+
+// const: Declares a constant variable (its value can't be reassigned).
+const age = 18;
+
+// var: Older way of declaring variables (less common now).
+var mark = 80.34;
+
+//Print details
+console.log(name);
+console.log(age);
+console.log(mark);
+
+//Data types in java scripts 
+
+// String: Text (e.g., "Hello")
+let String = "This is a String";
+
+// Number: Numeric values (e.g., 10, 3.14)
+let Number1 = 10
+let Number2 = 3.14;
+
+// Boolean: True or False (e.g., true, false)
+let boolean = false //Or ture 
+
+// Object: Collections of key-value pairs (e.g., { name: "John", age: 30 })
+let object = {name:"John",age:30};
+
+// Array: Ordered collections of values (e.g., [1, 2, 3])
+let array = [1,2,3];
+
+// Null: A special value representing no value or object
+console.log(null);
+
+// Undefined: A variable that has been declared but not assigned a value
+console.log(a) // a not definded in the programe so this will print undefinded
+
---
+++ operators.js
@@ -0,0 +1,31 @@
+// Operators are used to perform operations on variables and values.
+
+// Arithmetic operators: +, -, *, /, % (addition, subtraction, multiplication, division, modulus)
+let a = 10;
+let b = 24;
+let additionOperator = a + b;
+let subtractionOperator = a - b;
+let multiplicationOperator = a * b;
+let divisionOperator = a / b;
+let modulusOperator = a % b;
+console.log(additionOperator)
+console.log(subtractionOperator)
+console.log(multiplicationOperator)
+console.log(divisionOperator)
+console.log(modulusOperator)
+// Assignment operators: =, +=, -=, etc.
+let c = 10; // Here = is a Assignment operator
+c+=10; // add 10;
+c-=10; // subtraction  10;
+console.log(c);
+
+
+// Comparison operators: ==, ===, !=, >, <, >=, <= (return true or false to use in if else)
+
+console.log(a==b) // Check to values or equal
+console.log(a===b) // Check to values and data type
+console.log(a!=b) // True if both or not same
+console.log(a>b) // Check a is greter then b
+//..................etc
+// Logical operators: && (AND), || (OR), ! (NOT) see this on if else 
+
